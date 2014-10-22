381c86d697aadfc64d5f6d4981e5dee8772b2232
Quentin Lamerand <qlamerand@jahia.com>
2014-10-22 16:41:55 +0200

    BACKLOG-5328: Add unit tests

---
+++ test/src/main/java/org/jahia/test/external/users/ExternalUsersProviderTest.java
@@ -0,0 +1,162 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2014 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ======================================================================================
+ *
+ *     IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     "This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation; either version 2
+ *     of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *     As a special exception to the terms and conditions of version 2.0 of
+ *     the GPL (or any later version), you may redistribute this Program in connection
+ *     with Free/Libre and Open Source Software ("FLOSS") applications as described
+ *     in Jahia's FLOSS exception. You should have received a copy of the text
+ *     describing the FLOSS exception, also available here:
+ *     http://www.jahia.com/license"
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ======================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ *
+ *
+ * ==========================================================================================
+ * =                                   ABOUT JAHIA                                          =
+ * ==========================================================================================
+ *
+ *     Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
+ *     streamlining Enterprise digital projects across channels to truly control
+ *     time-to-market and TCO, project after project.
+ *     Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
+ *     marketing teams to collaboratively and iteratively build cutting-edge
+ *     online business solutions.
+ *     These, in turn, are securely and easily deployed as modules and apps,
+ *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ *     Each solution provided by Jahia stems from this overarching vision:
+ *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ *     Founded in 2002 and headquartered in Geneva, Switzerland,
+ *     Jahia Solutions Group has its North American headquarters in Washington DC,
+ *     with offices in Chicago, Toronto and throughout Europe.
+ *     Jahia counts hundreds of global brands and governmental organizations
+ *     among its loyal customers, in more than 20 countries across the globe.
+ *
+ *     For more information, please visit http://www.jahia.com
+ */
+package org.jahia.test.external.users;
+
+import com.google.common.collect.Sets;
+import org.jahia.modules.external.users.JCRExternalUserNode;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.decorator.JCRGroupNode;
+import org.jahia.services.content.decorator.JCRUserNode;
+import org.jahia.services.usermanager.JahiaGroupManagerService;
+import org.jahia.services.usermanager.JahiaUserManagerService;
+import org.jahia.test.JahiaTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jcr.RepositoryException;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Integration tests for the external users provider implementation.
+ * 
+ * @author Quentin Lamerand
+ */
+public class ExternalUsersProviderTest extends JahiaTestCase {
+
+    private JahiaUserManagerService jahiaUserManagerService;
+    private JahiaGroupManagerService jahiaGroupManagerService;
+
+    @Before
+    public void setUp() throws RepositoryException {
+        jahiaUserManagerService = JahiaUserManagerService.getInstance();
+        jahiaGroupManagerService = JahiaGroupManagerService.getInstance();
+    }
+
+    @Test
+    public void testLookupAndMembership() throws RepositoryException {
+        JCRUserNode tata = jahiaUserManagerService.lookupUser("tata");
+        assertNotNull("User should exist", tata);
+        JCRUserNode tete = jahiaUserManagerService.lookupUser("tete");
+        assertNotNull("User should exist", tete);
+        JCRUserNode titi = jahiaUserManagerService.lookupUser("titi");
+        assertNotNull("User should exist", titi);
+
+        JCRGroupNode toto = jahiaGroupManagerService.lookupGroup(null, "toto");
+        assertNotNull(toto);
+        JCRGroupNode tutu = jahiaGroupManagerService.lookupGroup(null, "tutu");
+        assertNotNull(tutu);
+        JCRGroupNode tyty = jahiaGroupManagerService.lookupGroup(null, "tyty");
+        assertNotNull(tyty);
+
+        assertEquals("toto should have tata, tete and tutu as members", Sets.newHashSet(tata, tete, tutu), new HashSet<JCRNodeWrapper>(toto.getMembers()));
+        assertEquals("tutu should have titi as member", Sets.newHashSet(titi), new HashSet<JCRNodeWrapper>(tutu.getMembers()));
+        assertEquals("tyty should have tete and titi as members", Sets.newHashSet(tete, titi), new HashSet<JCRNodeWrapper>(tyty.getMembers()));
+
+        assertEquals("titi should be a member of tutu, toto, tyty, users and guests", Sets.newHashSet(tutu.getPath(), toto.getPath(), tyty.getPath(), "/groups/users", "/groups/guest"), new HashSet<String>(jahiaGroupManagerService.getMembershipByPath(titi.getPath())));
+    }
+
+    @Test
+    public void testPassword() throws RepositoryException {
+        JCRUserNode tata = jahiaUserManagerService.lookupUser("tata");
+        assertNotNull("User should exist", tata);
+        assertTrue("User should be external", tata instanceof JCRExternalUserNode);
+        assertTrue(tata.verifyPassword("password"));
+    }
+
+    @Test
+    public void testSearch() throws RepositoryException {
+        JCRUserNode tata = jahiaUserManagerService.lookupUser("tata");
+        JCRUserNode tete = jahiaUserManagerService.lookupUser("tete");
+        JCRUserNode titi = jahiaUserManagerService.lookupUser("titi");
+
+        Properties properties = new Properties();
+        properties.put("username", "t*");
+        Set<JCRUserNode> users = jahiaUserManagerService.searchUsers(properties);
+        assertEquals("'username=t*' search should return tata, tete and titi", Sets.newHashSet(tata, tete, titi), users);
+
+        JCRGroupNode toto = jahiaGroupManagerService.lookupGroup(null, "toto");
+        JCRGroupNode tutu = jahiaGroupManagerService.lookupGroup(null, "tutu");
+        JCRGroupNode tyty = jahiaGroupManagerService.lookupGroup(null, "tyty");
+
+        properties = new Properties();
+        properties.put("groupname", "t*");
+        Set<JCRGroupNode> groups = jahiaGroupManagerService.searchGroups(null, properties);
+        assertEquals("'groupname=t*' search should return toto, tutu and tyty", Sets.newHashSet(toto, tutu, tyty), groups);
+    }
+
+}
---
+++ test/src/main/java/org/jahia/test/external/users/TxtxUserGroupProvider.java
@@ -0,0 +1,131 @@
+package org.jahia.test.external.users;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.modules.external.users.*;
+import org.jahia.services.usermanager.JahiaGroup;
+import org.jahia.services.usermanager.JahiaGroupImpl;
+import org.jahia.services.usermanager.JahiaUser;
+import org.jahia.services.usermanager.JahiaUserImpl;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class TxtxUserGroupProvider implements UserGroupProvider {
+
+    public static final String TXTX_PROVIDER_KEY = "txtx";
+
+    private List users = Arrays.asList("tata", "tete", "titi");
+
+    private List<String> groups = Arrays.asList("toto", "tutu", "tyty");
+
+    private ExternalUserGroupService externalUserGroupService;
+
+    @Override
+    public JahiaUser getUser(String name) throws UserNotFoundException {
+        if (users.contains(name)) {
+            Properties properties = new Properties();
+            properties.put("j:email", "mail@tx.tx");
+            return new JahiaUserImpl(name, name, properties, false, TXTX_PROVIDER_KEY);
+        }
+        throw new UserNotFoundException("Cannot find user " + name);
+    }
+
+    @Override
+    public JahiaGroup getGroup(String name) throws GroupNotFoundException {
+        if (groups.contains(name)) {
+            Properties properties = new Properties();
+            return new JahiaGroupImpl(name, name, null, properties);
+        }
+        throw new GroupNotFoundException("Cannot find group " + name);
+    }
+
+    @Override
+    public List<Member> getGroupMembers(String groupName) {
+        ArrayList<Member> members = new ArrayList<Member>();
+        if ("toto".equals(groupName)) {
+            members.add(new Member("tata", Member.MemberType.USER));
+            members.add(new Member("tete", Member.MemberType.USER));
+            members.add(new Member("tutu", Member.MemberType.GROUP));
+        }
+        if ("tutu".equals(groupName)) {
+            members.add(new Member("titi", Member.MemberType.USER));
+        }
+        if ("tyty".equals(groupName)) {
+            members.add(new Member("tete", Member.MemberType.USER));
+            members.add(new Member("titi", Member.MemberType.USER));
+        }
+        return members;
+    }
+
+    @Override
+    public List<String> getMembership(Member member) {
+        if ("tata".equals(member.getName())) {
+            return Arrays.asList("toto");
+        } else if ("tete".equals(member.getName())) {
+            return Arrays.asList("toto", "tyty");
+        } else if ("titi".equals(member.getName())) {
+            return Arrays.asList("tutu", "tyty");
+        } else if ("toto".equals(member.getName())) {
+            return Collections.emptyList();
+        } else if ("tutu".equals(member.getName())) {
+            return Arrays.asList("toto");
+        } else if ("tyty".equals(member.getName())) {
+            return Collections.emptyList();
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> searchUsers(Properties searchCriterias, long offset, long limit) {
+        String filter = (String) searchCriterias.get("username");
+        if (filter == null) {
+            filter = (String) searchCriterias.get("*");
+        }
+        ArrayList<String> l;
+        if (filter != null) {
+            l = new ArrayList<String>(Collections2.filter(users, Predicates.contains(Pattern.compile("^" + StringUtils.replace(filter, "*", ".*") + "$"))));
+        } else {
+            l = new ArrayList<String>(users);
+        }
+        return l.subList(Math.min((int) offset, l.size()), limit < 0 ? l.size() : Math.min((int) (offset + limit), l.size()));
+    }
+
+    @Override
+    public List<String> searchGroups(Properties searchCriterias, long offset, long limit) {
+        String filter = (String) searchCriterias.get("groupname");
+        if (filter == null) {
+            filter = (String) searchCriterias.get("*");
+        }
+        ArrayList<String> l;
+        if (filter != null) {
+            l = new ArrayList<String>(Collections2.filter(groups, Predicates.contains(Pattern.compile("^" + StringUtils.replace(filter, "*", ".*") + "$"))));
+        } else {
+            l = new ArrayList<String>(groups);
+        }
+        return l.subList(Math.min((int) offset, l.size()), limit < 0 ? l.size() : Math.min((int) (offset + limit), l.size()));
+    }
+
+    @Override
+    public boolean verifyPassword(String userName, String userPassword) {
+        return "password".equals(userPassword);
+    }
+
+    @Override
+    public boolean supportsGroups() {
+        return true;
+    }
+
+    public void init() {
+        externalUserGroupService.register("txtx", this);
+    }
+
+    public void destroy() {
+        externalUserGroupService.unregister("txtx");
+    }
+
+    public void setExternalUserGroupService(ExternalUserGroupService externalUserGroupService) {
+        this.externalUserGroupService = externalUserGroupService;
+    }
+}
